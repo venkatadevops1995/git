061fd5d639e0f89f8494e4c8a250d77a5145ccbd
Ubuntu <ubuntu@ip-172-31-29-100.ec2.internal>
2024-05-29 11:27:04 +0000

    first commit

---
+++ script.sh
@@ -0,0 +1,22 @@
+sudo apt-get update
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+
+sudo apt-get update
+
+
+sudo apt-get install -y fontconfig openjdk-17-jre
+
+sudo apt-get install -y jenkins
+
+
+sudo systemctl start jenkins
+
+
+sudo systemctl enable jenkins
+
+sudo apt-get install -y maven
